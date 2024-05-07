8779269755c9de922d101a1364a7c9ba8cdb2bf7
Yuli Currea <ycurrea@unbosque.edu.co>
2024-05-06 19:45:03 -0500

    c

--- src/app/components/menu/administrador/consulta/dialogo-estadistica/dialogo-estadistica.component.ts
+++ src/app/components/menu/administrador/consulta/dialogo-estadistica/dialogo-estadistica.component.ts
@@ -56,14 +56,14 @@ export class DialogoEstadisticaComponent implements OnInit {
     let labelData: string[] = [];
     switch(this.type) { 
       case 'Proyectos': {
-        this.data.forEach((element: { estadoProyectoObj: any; }) => {
-          labelData.push(element.estadoProyectoObj);
+        this.data.forEach((element: { estadosProyectos: any; }) => {
+          labelData.push(element.estadosProyectos);
         });
         break; 
       } 
       case 'Productos': {
-        this.data.forEach((element: { estadoProductoObj: any; }) => {
-          labelData.push(element.estadoProductoObj);
+        this.data.forEach((element: { estadosProductos: any; }) => {
+          labelData.push(element.estadosProductos);
         });
         break; 
       } 
