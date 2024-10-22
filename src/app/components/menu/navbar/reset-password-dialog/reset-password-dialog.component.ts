211d187d5fe743831b3bb1bef8430a2589536813
Yuli Currea <ycurrea@unbosque.edu.co>
2024-10-21 21:38:58 -0500

    cambios

--- src/app/components/menu/navbar/reset-password-dialog/reset-password-dialog.component.ts
+++ src/app/components/menu/navbar/reset-password-dialog/reset-password-dialog.component.ts
@@ -5,7 +5,7 @@ import Swal from 'sweetalert2';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-//formulario de ingresar el correo electronico
+  // formulario de ingresar el correo electronico
   selector: 'app-reset-password-dialog',
   template: `
     <h2 mat-dialog-title>Restablecer contraseña</h2>
@@ -18,25 +18,34 @@ import { MatDialogRef } from '@angular/material/dialog';
       </mat-dialog-content>
       <mat-dialog-actions>
         <button mat-button type="button" (click)="closeDialog()">Cancelar</button>
-        <button type="submit" mat-raised-button color="primary">Enviar</button>
+        <button type="submit" mat-raised-button color="primary" [disabled]="isLoading || resetInProgress">Enviar</button>
       </mat-dialog-actions>
+      <div *ngIf="isLoading">Cargando...</div> <!-- Mensaje de carga -->
     </form>
   `,
 })
 export class ResetPasswordDialogComponent {
   resetPasswordForm: FormGroup;
+  isLoading = false; // Variable para controlar el estado de carga
+  resetInProgress = false; // Variable para controlar si ya se ha iniciado un envío
 
   constructor(private fb: FormBuilder, private authService: AutenticacionService, private dialogRef: MatDialogRef<ResetPasswordDialogComponent>) {
     this.resetPasswordForm = this.fb.group({
       correo: ['', [Validators.required, Validators.email]],
     });
   }
-//Valida el formulario y llama al servicio de autenticación para enviar la solicitud de restablecimiento de contraseña
+
+  // Valida el formulario y llama al servicio de autenticación para enviar la solicitud de restablecimiento de contraseña
   resetPassword() {
-    if (this.resetPasswordForm.valid) {
+    if (this.resetPasswordForm.valid && !this.resetInProgress) {
+      this.isLoading = true; // Iniciar carga
+      this.resetInProgress = true; // Evitar nuevos envíos
       const correo = this.resetPasswordForm.get('correo')?.value;
+      
       this.authService.resetPassword(correo).subscribe(
         () => {
+          this.isLoading = false; // Finalizar carga
+          this.resetInProgress = false; // Permitir nuevos envíos
           Swal.fire({
             icon: 'success',
             title: '¡Éxito!',
@@ -45,6 +54,8 @@ export class ResetPasswordDialogComponent {
           this.dialogRef.close();
         },
         (error) => {
+          this.isLoading = false; // Finalizar carga
+          this.resetInProgress = false; // Permitir nuevos envíos
           Swal.fire({
             icon: 'error',
             title: 'Error',
